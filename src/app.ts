dcda114a880126d08e02eaf649ea523093ffc40c
Michael <justy031090@gmail.com>
2022-07-04 03:13:15 +0300

    rendering project items

--- src/app.ts
+++ src/app.ts
@@ -148,6 +148,23 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+//project item class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    configure() {}
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent =
+            this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
 //project list class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
@@ -165,9 +182,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         )! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.id, prjItem);
         }
     }
     configure() {
