b7785bdae264a9fa2a2b41026813806410b60d7e
Michael <justy031090@gmail.com>
2022-09-05 19:53:59 +0300

    setting

---
+++ jbook/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const onClick = () => {
+        console.log(input);
+    };
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
+
+export default App;
