d0b2f724f13b3bd786bca3a74ff079375cce94fe
Michael <justy031090@gmail.com>
2022-09-05 21:57:49 +0300

    transpiling

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -18,9 +18,13 @@ const App = () => {
         startService();
     }, []);
 
-    const onClick = () => {
+    const onClick = async () => {
         if (!ref.current) return;
-        console.log(ref.current);
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        });
+        setCode(result.code);
     };
 
     return (
