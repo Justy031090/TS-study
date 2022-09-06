1eff6de36ceb2efe810049ae533e6297099f3f42
Michael <justy031090@gmail.com>
2022-09-06 21:57:00 +0300

    loading from unpkg

--- jbook/src/plugins/unpkg-path-plugin.tsx
+++ jbook/src/plugins/unpkg-path-plugin.tsx
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
     //what the path to the file is --> onResolve
@@ -6,30 +7,34 @@ export const unpkgPathPlugin = () => {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log('onResole', args);
-                return { path: args.path, namespace: 'a' };
+                console.log('onResolve', args);
+                if (args.path === 'index.js')
+                    return { path: args.path, namespace: 'a' };
+                else if (args.path === 'tiny-test-pkg')
+                    return {
+                        path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+                        namespace: 'a',
+                    };
             });
             //trying to load the file the the onResolve step (onLoad)
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 console.log('onLoad', args);
-
                 //instead of loading index.js through the file system we return the contents of the file that esbuild was looking for.
-
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
                         contents: `
-            import message from './message';
+            import message from 'tiny-test-pkg';
             console.log(message);
             `,
                         //if esbuild finds import statements, it goes again to a cycle of onResolve-->onLoad
                     };
-                } else {
-                    return {
-                        loader: 'jsx',
-                        contents: 'export default "hi there!"',
-                    };
                 }
+                const { data } = await axios.get(args.path);
+                return {
+                    loader: 'jsx',
+                    contents: data,
+                };
             });
         },
     };
