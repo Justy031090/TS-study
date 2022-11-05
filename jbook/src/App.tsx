a1003ec0ab37d1f43fc1140a677a572c12a2a977
Michael <justy031090@gmail.com>
2022-11-05 13:25:05 +0200

    added trycatch block for bad code handle

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -47,7 +47,14 @@ const App = () => {
                     <div id="root"></div>
                     <script>
                         window.addEventListener('message', (event)=>{
-                            eval(event.data)
+                            try {
+                                eval(event.data)
+                            }catch(err){
+                                const root = document.querySelector('#root')
+                                root.innerHTML = '<div style="color: red";> <h4>Error</h4>' + err + '</div>'
+                                console.error(err);
+                            }
+                            
                         }, false);
                     </script>
                 </body>
