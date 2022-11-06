28fae1a768f1150c8738c3f42c933bf113f710e3
Michael <justy031090@gmail.com>
2022-11-06 17:41:46 +0200

    Code editor options added

--- jbook/src/components/CodeEditor.tsx
+++ jbook/src/components/CodeEditor.tsx
@@ -6,7 +6,15 @@ const CodeEditor = () => {
             height="500px"
             language="javascript"
             theme="dark"
-            options={{ wordWrap: 'on' }}
+            options={{
+                wordWrap: 'on',
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+            }}
         />
     );
 };
