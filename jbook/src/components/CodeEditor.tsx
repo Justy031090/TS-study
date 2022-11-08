4c1a1220d4aa30e479e8064e84930c5a1ee1d3d1
Michael <justy031090@gmail.com>
2022-11-08 08:43:30 +0200

    styling added

--- jbook/src/components/CodeEditor.tsx
+++ jbook/src/components/CodeEditor.tsx
@@ -1,3 +1,4 @@
+import './code-editor.css';
 import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
@@ -21,19 +22,26 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
 
     const onFormatClick = () => {
         const unformatted = editorRef.current.getModel().getValue();
-        const formatted = prettier.format(unformatted, {
-            parser: 'babel',
-            plugins: [parser],
-            useTabs: false,
-            semi: true,
-            singleQuote: true,
-        });
+        const formatted = prettier
+            .format(unformatted, {
+                parser: 'babel',
+                plugins: [parser],
+                useTabs: false,
+                semi: true,
+                singleQuote: true,
+            })
+            .replace(/\n$/, '');
         editorRef.current.setValue(formatted);
     };
 
     return (
-        <div>
-            <button onClick={onFormatClick}>Format</button>
+        <div className="editor-wrapper">
+            <button
+                onClick={onFormatClick}
+                className="button button-format is-primary is-small"
+            >
+                Format
+            </button>
             <MonacoEditor
                 editorDidMount={onEditorDidMount}
                 value={initialValue}
