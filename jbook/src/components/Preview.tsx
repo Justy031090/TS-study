d4fb388a1c77e27411fd868681740bdead48c2a6
Michael <justy031090@gmail.com>
2022-11-09 23:41:45 +0200

    fixed head positioning + fixed issue with iframe-resize element background

--- jbook/src/components/Preview.tsx
+++ jbook/src/components/Preview.tsx
@@ -7,22 +7,23 @@ interface PreviewProps {
 const html = `
         <html>
             <head>
-                <body>
-                    <div id="root"></div>
-                    <script>
-                        window.addEventListener('message', (event)=>{
-                            try {
-                                eval(event.data)
-                            }catch(err){
-                                const root = document.querySelector('#root')
-                                root.innerHTML = '<div style="color: red";> <h4>Error</h4>' + err + '</div>'
-                                console.error(err);
-                            }
-                            
-                        }, false);
-                    </script>
-                </body>
+            <style> html {background-color: white;}</style>
             </head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event)=>{
+                        try {
+                            eval(event.data)
+                        }catch(err){
+                            const root = document.querySelector('#root')
+                            root.innerHTML = '<div style="color: red";> <h4>Error</h4>' + err + '</div>'
+                            console.error(err);
+                        }    
+                    }, false);
+                </script>
+            </body>
+            
         </html>    
     `;
 const Preview: React.FC<PreviewProps> = ({ code }) => {
