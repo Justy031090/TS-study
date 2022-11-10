24b95b572122b1a980fa18b9dd8724cca5f39db4
Michael <justy031090@gmail.com>
2022-11-10 16:32:12 +0200

    conditional toggling for the mrkdwn

--- jbook/src/App.tsx
+++ jbook/src/App.tsx
@@ -1,10 +1,10 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 
 const App = () => {
     return (
         <div>
-            <CodeCell />
+            <TextEditor />
         </div>
     );
 };
---
+++ jbook/src/components/TextEditor.tsx
@@ -0,0 +1,39 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useState, useEffect, useRef } from 'react';
+
+const TextEditor: React.FC = () => {
+    const [editing, setEditing] = useState(false);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const listener = (event: MouseEvent) => {
+            if (
+                ref.current &&
+                event.target &&
+                ref.current.contains(event.target as Node)
+            ) {
+                return;
+            }
+            setEditing(false);
+        };
+        document.addEventListener('click', listener, { capture: true });
+        return () => {
+            document.removeEventListener('click', listener, { capture: true });
+        };
+    }, []);
+
+    if (editing) {
+        return (
+            <div ref={ref}>
+                <MDEditor />
+            </div>
+        );
+    }
+    return (
+        <div onClick={() => setEditing(true)}>
+            <MDEditor.Markdown source={'# header'} />
+        </div>
+    );
+};
+
+export default TextEditor;
