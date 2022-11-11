a7bcd2df8958f5d3312d797ffc73deeb48e15f47
Michael <justy031090@gmail.com>
2022-11-11 23:09:41 +0200

    wiring redux

---
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -0,0 +1,39 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellState {
+    loading: boolean;
+    error: string | null;
+    order: string[];
+    data: {
+        [key: string]: Cell;
+    };
+}
+
+const initialState: CellState = {
+    loading: false,
+    error: null,
+    order: [],
+    data: {},
+};
+
+const reducer = (
+    state: CellState = initialState,
+    action: Action
+): CellState => {
+    switch (action.type) {
+        case ActionType.UPDATE_CELL:
+            return state;
+        case ActionType.DELETE_CELL:
+            return state;
+        case ActionType.MOVE_CELL:
+            return state;
+        case ActionType.INSERT_CELL_BEFORE:
+            return state;
+        default:
+            return state;
+    }
+};
+
+export default reducer;
