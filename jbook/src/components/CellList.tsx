cf2a83256dcf93dba675678f5e06a34095712b51
Michael <justy031090@gmail.com>
2022-11-12 18:35:16 +0200

    rendering cells list

--- jbook/src/components/CellList.tsx
+++ jbook/src/components/CellList.tsx
@@ -1,8 +1,16 @@
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import CellListItem from './CellListItem';
 
 const CellList: React.FC = () => {
-    useTypedSelector((state) => state);
-    return <div>CellList</div>;
+    const cells = useTypedSelector(({ cells: { order, data } }) => {
+        return order.map((id) => data[id]);
+    });
+
+    const renderedCells = cells.map((cell) => (
+        <CellListItem cell={cell} key={cell.id} />
+    ));
+
+    return <div>{renderedCells}</div>;
 };
 
 export default CellList;
--- jbook/src/components/CellListItem.tsx
+++ jbook/src/components/CellListItem.tsx
@@ -1,6 +1,18 @@
+import { Cell } from '../state';
+import CodeCell from './CodeCell';
+import TextEditor from './TextEditor';
 
-const CellListItem: React.FC = () => {
-    return <div>CellListItem</div>;
+interface CellListItemProps {
+    cell: Cell;
+}
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+    let child: JSX.Element;
+    if (cell.type === 'code') {
+        child = <CodeCell />;
+    } else {
+        child = <TextEditor />;
+    }
+    return <div>{child}</div>;
 };
 
 export default CellListItem;
