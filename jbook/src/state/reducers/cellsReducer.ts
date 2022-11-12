d1b00ddb582348f9dda8c68b2f21fbab19521260
Michael <justy031090@gmail.com>
2022-11-12 03:17:51 +0200

    swapping cells logic

--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -26,9 +26,22 @@ const reducer = produce((state: CellState = initialState, action: Action) => {
             state.data[id].content = content;
             return;
         case ActionType.DELETE_CELL:
-            return state;
+            delete state.data[action.payload];
+            state.order = state.order.filter((id) => id !== action.payload);
+            return;
         case ActionType.MOVE_CELL:
-            return state;
+            const { direction } = action.payload;
+            const index = state.order.findIndex(
+                (id) => id === action.payload.id
+            );
+            const targetIndex = direction === 'up' ? index - 1 : index + 1;
+            if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+                return;
+            }
+            state.order[index] = state.order[targetIndex];
+            state.order[targetIndex] = action.payload.id;
+
+            return;
         case ActionType.INSERT_CELL_BEFORE:
             return state;
         default:
