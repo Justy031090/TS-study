da28bacabd61712732bf765c8929e4ffd138b806
Michael <justy031090@gmail.com>
2022-11-15 16:52:59 +0200

    adding vertical bar spacing

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -31,7 +31,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         <Resizable direction="vertical">
             <div
                 style={{
-                    height: '100%',
+                    height: 'calc(100% - 10px)',
                     display: 'flex',
                     flexDirection: 'row',
                 }}
