4242779128957bd9560cec4b67b0135a173b51e0
Michael <justy031090@gmail.com>
2022-11-17 14:16:14 +0200

    added progress bar & animation , background flickering fix & conditional rendering when bundleing

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from './CodeEditor';
 import Preview from './Preview';
@@ -44,11 +45,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {!bundle || bundle.loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <Preview code={bundle.code} err={bundle.err} />
-                )}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            {' '}
+                            <progress
+                                className="progress is-small is-primary"
+                                max="100"
+                            >
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
