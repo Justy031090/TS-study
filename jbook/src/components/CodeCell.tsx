6c57b637638d6adf21342ce7b83afdb23a682e92
Michael <justy031090@gmail.com>
2022-11-18 19:48:50 +0200

    added cumulative code selector & refactor for cumulative code

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -15,20 +15,35 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
 
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useTypedSelector((state) => {
+        const { data, order } = state.cells;
+        const orderedCells = order.map((id) => data[id]);
+        const cumulativeCode = [];
+        for (let c of orderedCells) {
+            if (c.type === 'code') {
+                cumulativeCode.push(c.content);
+            }
+            if (c.id === cell.id) {
+                break;
+            }
+        }
+        return cumulativeCode;
+    });
+    console.log(cumulativeCode);
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join('\n'));
             return;
         }
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join('\n'));
         }, 1000);
         return () => {
             clearTimeout(timer);
         };
         //eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cell.id, cell.content, createBundle]);
+    }, [cell.id, cumulativeCode.join('\n'), createBundle]);
 
     return (
         <Resizable direction="vertical">
