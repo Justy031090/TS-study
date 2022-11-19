7d45b115ba677e439d892cb881116f887147a634
Michael <justy031090@gmail.com>
2022-11-19 11:44:56 +0200

    selective to cell show call

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -18,26 +18,31 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const cumulativeCode = useTypedSelector((state) => {
         const { data, order } = state.cells;
         const orderedCells = order.map((id) => data[id]);
-        const cumulativeCode = [
-            `
-            import _React from 'react';
-            import _ReactDOM from 'react-dom';
-            const show = (value) => { 
-                const root = document.querySelector('#root')
+        const showFunc = `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        var show = (value) => { 
+            const root = document.querySelector('#root')
 
-                if (typeof value === 'object'){
-                    if(value.$$typeof && value.props){
-                        _ReactDOM.render(value, root)
-                    } else {
-                    root.innerHTML = JSON.stringify(value);
-                    }
+            if (typeof value === 'object'){
+                if(value.$$typeof && value.props){
+                    _ReactDOM.render(value, root)
                 } else {
-                    root.innerHTML = value;
+                root.innerHTML = JSON.stringify(value);
                 }
-            }`,
-        ];
+            } else {
+                root.innerHTML = value;
+            }
+        }`;
+        const showFuncNoOp = 'var show = () => {}';
+        const cumulativeCode = [];
         for (let c of orderedCells) {
             if (c.type === 'code') {
+                if (c.id === cell.id) {
+                    cumulativeCode.push(showFunc);
+                } else {
+                    cumulativeCode.push(showFuncNoOp);
+                }
                 cumulativeCode.push(c.content);
             }
             if (c.id === cell.id) {
