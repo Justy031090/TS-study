76fe9d13fe51c3bd1d1e7b862dae047d3e47e22f
Michael <justy031090@gmail.com>
2022-11-21 20:14:06 +0200

    adding serve command & port option

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,8 +1,9 @@
 import { Command } from 'commander';
 
 export const serveCommand = new Command()
-    .command('serve')
+    .command('serve [filename]')
     .description('Open a file for editing')
-    .action(() => {
-        console.log('---MEH---');
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action((filename = 'notebook.js', options) => {
+        console.log(filename, options);
     });
