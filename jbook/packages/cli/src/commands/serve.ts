7dc9c2b8d0032d4000692635b142937830f68020
Michael <justy031090@gmail.com>
2022-11-22 20:50:03 +0200

    adding file paths

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
 
@@ -6,5 +7,7 @@ export const serveCommand = new Command()
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action((filename = 'notebook.js', options) => {
-        serve(parseInt(options.port), filename, '/');
+        const dir = path.join(process.cwd(), path.dirname(filename));
+
+        serve(parseInt(options.port), path.basename(filename), dir);
     });
