dbe82ac63fda6221ff2cfcd8bb438df48ddf28f8
Michael <justy031090@gmail.com>
2022-11-22 20:39:52 +0200

    communication from cli to api

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,9 +1,10 @@
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action((filename = 'notebook.js', options) => {
-        console.log(filename, options);
+        serve(parseInt(options.port), filename, '/');
     });
--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log('Server is listening');
+export const serve = (port: number, filename: string, dir: string) => {
+    console.log('Serve traffic on port', port);
+    console.log('fetching cells from', filename);
+    console.log('file in dir', dir);
 };
